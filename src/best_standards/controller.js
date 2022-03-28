6b80bfa1277a535a163b92c4b08c7a36106cebd5
rick-rodriguez16 <rickrod6502@gmail.com>
2022-03-27 23:53:47 -0400

    added createBenchmark

--- src/best_standards/controller.js
+++ src/best_standards/controller.js
@@ -2,7 +2,7 @@ const pool = require('../../db.js');
 const queries = require("./queries.js")
 
 
-const getStandards = (req, res) => {
+const getAllStandards = (req, res) => {
     pool.query(queries.getAllStandards, (err, results) => {
         if (err) throw err;
         res.status(200).json(results.rows);
@@ -17,7 +17,26 @@ const getStandardByFullStandard = (req, res) => {
 };
 
 
-const getBenchmarks = (req, res) => {
+
+const createBenchmark = (req, res) => {
+    const {full_benchmark, benchmark_def, example1, example2, example3, clarification1, clarification2, clarification3, clarification4, purpose, misconception1, misconception2, misconception3, misconception4, misconception5, terms, full_standard, emphasis1, emphasis2, emphasis3, emphasis4, emphasis5, benchmark_no} = req.body;
+    
+    // check if benchmark already exists
+    pool.query(queries.getBenchmarkByFullBenchmark, [full_benchmark], (error, results) => {
+        
+        if (results.rows.length) {                          // if array length returned > 0, benchmark exists and cannot be added
+            res.send("Sorry, benchmark already exists.");
+        }
+        else {                                              
+            pool.query(queries.createBenchmark, [full_benchmark, benchmark_def, example1, example2, example3, clarification1, clarification2, clarification3, clarification4, purpose, misconception1, misconception2, misconception3, misconception4, misconception5, terms, full_standard, emphasis1, emphasis2, emphasis3, emphasis4, emphasis5, benchmark_no], (error, results) => {
+                if (error) throw error;
+                res.status(201).send("Benchmark successfully created.");
+            });
+        }
+    });
+};
+
+const getAllBenchmarks = (req, res) => {
     pool.query(queries.getAllBenchmarks, (err, results) => {
         if (err) throw err;
         res.status(200).json(results.rows);
@@ -33,8 +52,9 @@ const getBenchmarkByFullBenchmark = (req, res) => {
 };
 
 module.exports = {
-    getStandards,
+    getAllStandards,
     getStandardByFullStandard,
-    getBenchmarks,
+    createBenchmark,
+    getAllBenchmarks,
     getBenchmarkByFullBenchmark,
 }
--- src/best_standards/routes.js
+++ src/best_standards/routes.js
@@ -3,10 +3,13 @@ const router = Router();
 
 const controller = require('./controller');
 
-router.get('/standards', controller.getStandards);
+router.get('/standards', controller.getAllStandards);
 router.get('/standards/:full_standard', controller.getStandardByFullStandard);
 
-router.get('/benchmarks', controller.getBenchmarks);
+
+router.post('/benchmarks', controller.createBenchmark);
+router.get('/benchmarks', controller.getAllBenchmarks);
 router.get('/benchmarks/:full_benchmark', controller.getBenchmarkByFullBenchmark);
+
 
 module.exports = router;
