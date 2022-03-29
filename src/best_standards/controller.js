7cf36e940f6ee863833c567b118154abe7a68aba
rick-rodriguez16 <rickrod6502@gmail.com>
2022-03-28 23:17:00 -0400

    added standard/benchmark creation/deletion

--- src/best_standards/controller.js
+++ src/best_standards/controller.js
@@ -2,6 +2,24 @@ const pool = require('../../db.js');
 const queries = require("./queries.js")
 
 
+const createStandard = (req, res) => {
+    const {full_standard, standard_def, content, content_name, grade, strand, strand_def, standard_no} = req.body;
+    
+    // check if standard already exists
+    pool.query(queries.getStandardByFullStandard, [full_standard], (error, results) => {
+        
+        if (results.rows.length) {                          // if array length returned > 0, benchmark exists and cannot be added
+            res.send("Sorry, standard already exists.");
+            return;
+        }
+
+        pool.query(queries.createStandard, [full_standard, standard_def, content, content_name, grade, strand, strand_def, standard_no], (error, results) => {
+            if (error) throw error;
+            res.status(201).send("Standard successfully created.");
+        });
+    });
+};
+
 const getAllStandards = (req, res) => {
     pool.query(queries.getAllStandards, (err, results) => {
         if (err) throw err;
@@ -16,6 +34,22 @@ const getStandardByFullStandard = (req, res) => {
     });
 };
 
+const deleteStandard = (req, res) => {
+    const standard = req.params.full_standard;    
+    pool.query(queries.getStandardByFullStandard, [standard], (error, results) => {
+        
+        if (!results.rows.length) {                          // if the standard does not exist...
+            res.send("Sorry, standard does not exist.");
+            return;
+        }
+    
+        pool.query(queries.deleteStandard, [standard], (err, results) => {
+            if (err) throw err;
+            res.status(200).send("Standard successfully deleted.");
+        });
+    });
+};
+
 
 
 const createBenchmark = (req, res) => {
@@ -26,13 +60,13 @@ const createBenchmark = (req, res) => {
         
         if (results.rows.length) {                          // if array length returned > 0, benchmark exists and cannot be added
             res.send("Sorry, benchmark already exists.");
+            return;
         }
-        else {                                              
-            pool.query(queries.createBenchmark, [full_benchmark, benchmark_def, example1, example2, example3, clarification1, clarification2, clarification3, clarification4, purpose, misconception1, misconception2, misconception3, misconception4, misconception5, terms, full_standard, emphasis1, emphasis2, emphasis3, emphasis4, emphasis5, benchmark_no], (error, results) => {
-                if (error) throw error;
-                res.status(201).send("Benchmark successfully created.");
-            });
-        }
+
+        pool.query(queries.createBenchmark, [full_benchmark, benchmark_def, example1, example2, example3, clarification1, clarification2, clarification3, clarification4, purpose, misconception1, misconception2, misconception3, misconception4, misconception5, terms, full_standard, emphasis1, emphasis2, emphasis3, emphasis4, emphasis5, benchmark_no], (error, results) => {
+            if (error) throw error;
+            res.status(201).send("Benchmark successfully created.");
+        });
     });
 };
 
@@ -51,10 +85,29 @@ const getBenchmarkByFullBenchmark = (req, res) => {
     });
 };
 
+const deleteBenchmark = (req, res) => {
+    const benchmark = req.params.full_benchmark;    
+    pool.query(queries.getBenchmarkByFullBenchmark, [benchmark], (error, results) => {
+        
+        if (!results.rows.length) {                          // if the benchmark does not exist...
+            res.send("Sorry, benchmark does not exist.");
+            return;
+        }
+    
+        pool.query(queries.deleteBenchmark, [benchmark], (err, results) => {
+            if (err) throw err;
+            res.status(200).send("Benchmark successfully deleted.");
+        });
+    });
+};
+
 module.exports = {
     getAllStandards,
     getStandardByFullStandard,
+    createStandard,
+    deleteStandard,
     createBenchmark,
     getAllBenchmarks,
     getBenchmarkByFullBenchmark,
+    deleteBenchmark,
 }
--- src/best_standards/routes.js
+++ src/best_standards/routes.js
@@ -3,13 +3,16 @@ const router = Router();
 
 const controller = require('./controller');
 
+router.post('/standards', controller.createStandard);
 router.get('/standards', controller.getAllStandards);
 router.get('/standards/:full_standard', controller.getStandardByFullStandard);
+router.delete('/standards/:full_standard', controller.deleteStandard);
 
 
 router.post('/benchmarks', controller.createBenchmark);
 router.get('/benchmarks', controller.getAllBenchmarks);
 router.get('/benchmarks/:full_benchmark', controller.getBenchmarkByFullBenchmark);
+router.delete('/benchmarks/:full_benchmark', controller.deleteBenchmark);
 
 
 module.exports = router;
